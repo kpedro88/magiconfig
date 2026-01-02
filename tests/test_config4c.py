a3296c8ea726115ff2c310b91605b570da2e6a32
Kevin Pedro <kpedro88@gmail.com>
2026-01-02 19:01:40 +0530

    missing test file from 2534152

---
+++ tests/test_config4c.py
@@ -0,0 +1,8 @@
+from magiconfig import MagiConfig
+from collections import OrderedDict
+
+config = MagiConfig()
+config.bar = 2
+config.foo = 2
+config.ipsum = False
+config.names = OrderedDict([("Alice",True)])
